59badd38a8425347da228707ed3a4be3dbc70a2e
Richard McClellan <richard.mcclellan@gmail.com>
2013-04-10 12:48:17 -0700

    added code to send push notifications after a new message is added

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,22 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+
+
+Parse.Cloud.afterSave("Message", function(request) {
+  query = new Parse.Query("Post");
+  query.get(request.object.get("post").id, {
+    success: function(post) {
+    	Parse.Push.send({
+	  		channels: [ "new_messages" ],
+	  		data: {
+	     		alert: "Quit Your Jibba Jabba"
+	  		});
+  		};
+  	}, 
+  	error: function(err) { 
+		console.log(err);
+	}
+  });
+});

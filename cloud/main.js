fc980a9e7eecb397164afec0877c0ba51e7664bd
Richard McClellan <richard.mcclellan@gmail.com>
2013-05-04 17:41:56 -0700

    cloud changes

--- cloud/main.js
+++ cloud/main.js
@@ -5,21 +5,62 @@ Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
 
-
-
-Parse.Cloud.afterSave("Message", function(request) {
-  query = new Parse.Query("Post");
-  query.get(request.object.get("post").id, {
-    success: function(post) {
-    	Parse.Push.send({
-	  		channels: [ "new_messages" ],
+Parse.Cloud.afterSave("Message", function(request) 
+{
+  query = new Parse.Query("Message");
+  query.get(request.object.id, 
+  {
+    success: function(object) 
+    {
+    	console.log(request);
+    	push_query = new Parse.Query("Installation");
+    	push_query = push_query.containsAll("channels", ["new_messages"]);
+		  installation = request.object.get("installation");
+    	push_query = push_query.notEqualTo("objectId", request.object.get("installation")["id"]);
+    	Parse.Push.send(
+    	{
+	  		// channels: [ "new_messages" ],
+	  		where: push_query,
 	  		data: {
-	     		alert: "Quit Your Jibba Jabba"
-	  		});
-  		};
-  	}, 
-  	error: function(err) { 
+	     		alert: request.object.get("text")
+	  		}
+  		},
+  		{ 
+  			success: function() 
+  			{
+  			},
+  			error: function(error)
+  			{
+  				console.log(err);
+  			}
+  		});
+  	},
+  	error: function(object, error) 
+  	{
 		console.log(err);
 	}
   });
+});
+
+Parse.Cloud.define("messages", function(request, response) {
+  query = new Parse.Query("Message");
+  query.find({
+    success: function(results) {
+      messages = results;
+    },
+
+    error: function(error) {
+      console.log(error);
+    }
+  });
+  query = new Parse.Query("Comment");
+  query.find({
+    success: function(results) {
+      comments = results;
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  });
+  response.success(messages);
 });
